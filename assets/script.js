f54940ab300584ec7c53cf84fee3ec46224a6054
Matt Dunston <matt.cdunston21@gmail.com>
2022-04-11 00:04:25 -0700

    added return value for generate password function and made the if else statements more effiecient

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,18 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 let characterLength;
-let choices; 
-const specialChar = [ "~", "!", "@", "#", "$", "%","^","&","*","(",")","-","_", ":",";",".","'","[","]","{","}","?",","];
-const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','u','v','w','x','y','z'];
-const numbers = ['1','2','3','4','5','6','7','8','9','0'];
+let generatedPassword = "";
+
+const specialCharArr = [ "~", "!", "@", "#", "$", "%","^","&","*","(",")","-","_", ":",";",".","'","[","]","{","}","?",","];
+const upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','u','v','w','x','y','z'];
+const numbersArr = ['1','2','3','4','5','6','7','8','9','0'];
+const choices = []
 
 
 
-//  function getUserSelection() {
-
-//  }
-
-function generatePassword () 
-{
-
-//parameters for user password length
+function generatePassword () {
+  //parameters for user password length
 characterLength = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
 if (!characterLength){
   alert("This needs a value. ");
@@ -24,25 +20,41 @@ if (!characterLength){
 } else if (characterLength < 8 || characterLength > 128){
   characterLength = parseInt(prompt("You must choose between 8 and 128"));
 } else {
-specialChar = window.confirm("Click OK if you want to have special characters in your password. ");
-upperCase = window.confirm("Click OK if you want uppercase letters to be in your password. ");
-lowerCase = window.confirm("Click OK if you want lowercase letters to be in your password. ");
-numbers = window.confirm("Click OK if you want numbers to be in your password. ");
-// scenarios in which user doesnt click OK
+const specialChar = window.confirm("Click OK if you want to have special characters in your password. ");
+const upperCase = window.confirm("Click OK if you want uppercase letters to be in your password. ");
+const lowerCase = window.confirm("Click OK if you want lowercase letters to be in your password. ");
+const numbers = window.confirm("Click OK if you want numbers to be in your password. ");
+//scenarios in which user doesnt click OK
 if (!specialChar && !upperCase && !lowerCase && !numbers){
+}
+  if (lowercase){
+      choices = choices.concat(lowerCase)
+    }
+    
+      if (uppercase) {
+        choices = choices.concat(upperCase)
+    }
+    
+      if (specialCharacters) {
+        choices = choices.concat(specialChar)
+    }
+    
+      if (number) {
+        choices = choices.concat(number)
+    }
+    
 
-} else if (specialChar && upperCase && lowerCase)
-{} else if (specialChar && upperCase)
-{} else if (specialChar)
-let thePassword =[];
 
 //loop to give random generated password 
-for (i = 0; i < chracterLength + 1; i++) {
-    let randomCharIndex = choices[ Math.floor(Math.random() * possibleCharacters.length)]
-    generatedPassword += possibleCharacters[randomCharIndex]  
-    thePassword.push(randomCharIndex)
-}} 
-//function to button so what when clicked, it performs the generatePassword function
+for (i = 0; i < characterLength + 1; i++) {
+    let randomCharIndex = choices[ Math.floor(Math.random() * choices.length)]
+    generatedPassword += choices[randomCharIndex]  
+  
+}
+return generatedPassword
+}
+
+}
 generateBtn.addEventListener("click", function () {
   passwordVar = generatePassword();
   document.getElementById("passwordVar");
