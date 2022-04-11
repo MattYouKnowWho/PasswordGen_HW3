883213e84d10c81924a62642f754e5c6c8f4ab61
Matt Dunston <matt.cdunston21@gmail.com>
2022-04-10 22:44:13 -0700

    added window.confirm for all arrays and added eventlister/loop

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,53 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-
-//making arrays 
+let characterLength;
+let choices; 
 const specialChar = [ "~", "!", "@", "#", "$", "%","^","&","*","(",")","-","_", ":",";",".","'","[","]","{","}","?",","];
 const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','u','v','w','x','y','z'];
-const number = ['1','2','3','4','5','6','7','8','9','0'];
+const numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
-let thePassword = "";
+
+
+//  function getUserSelection() {
+
+//  }
 
 function generatePassword () 
 {
 //prompting user for the length and keys desired for their password
-let characterLength = window.prompt("How keys between 8 and 128 characters would you like your password to be made up of?");
-let specialChar = window.confirm("Click OK if you want to have special characters in your password. ");
-let upperCase = window.confirm("Click OK if you want uppercase letters to be in your password. ")
-let lowerCase = window.confirm("Click OK if you want lowercase letters to be in your password. ")
-let number = window.confirm("Click OK if you want numbers to be in your password. ")
-}
+// characterLength = window.prompt("How keys between 8 and 128 characters would you like your password to be made up of?");
+// let specialChar = window.confirm("Click OK if you want to have special characters in your password. ");
+// let upperCase = window.confirm("Click OK if you want uppercase letters to be in your password. ");
+// let lowerCase = window.confirm("Click OK if you want lowercase letters to be in your password. ");
+// let number = window.confirm("Click OK if you want numbers to be in your password. ");
+//
 //parameters for user password length
-if (characterLength < 8 || characterLength > 128){
-  window.alert("password length has to be between 8 and 128 characters");
-}
+characterLength = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+if (!characterLength){
+  alert("This needs a value. ");
+
+} else if (characterLength < 8 || characterLength > 128){
+  characterLength = parseInt(prompt("You must choose between 8 and 128"));
+} else {
+specialChar = window.confirm("Click OK if you want to have special characters in your password. ");
+upperCase = window.confirm("Click OK if you want uppercase letters to be in your password. ");
+lowerCase = window.confirm("Click OK if you want lowercase letters to be in your password. ");
+numbers = window.confirm("Click OK if you want numbers to be in your password. ");
+if (!specialChar && !upperCase && !lowerCase && !numbers){
+
+} else if (specialChar && upperCase && lowerCase)
+{} else if (specialChar && upperCase)
+{} else if (specialChar)
+let thePassword =[];
+
+for (i = 0; i < chracterLength + 1; i++) {
+    let randomCharIndex = choices[ Math.floor(Math.random() * possibleCharacters.length)]
+    generatedPassword += possibleCharacters[randomCharIndex]  
+    thePassword.push(randomCharIndex)
+}} 
+
+generateBtn.addEventListener("click", function () {
+  passwordVar = generatePassword();
+  document.getElementById("passwordVar");
+})
